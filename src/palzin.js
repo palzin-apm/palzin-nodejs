b50bbee5a623194864fb3b045a5a34b0ac9e889f
Palzin <hello@palzin.com>
2023-07-12 14:23:57 +0530

    Fixed issue with Package

--- src/palzin.js
+++ src/palzin.js
@@ -10,7 +10,7 @@ class Palzin {
       url: "demo.palzin.app",
       ingestionKey: "",
       enabled: true,
-      version: "24.03.22",
+      version: "23.03.23",
       autoWiring: true,
       modules: [],
       maxEntries: 100,
